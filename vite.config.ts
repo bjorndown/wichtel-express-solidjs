f13ce0c3b8f7d48075e191d91055c6506b32ca4f
bjorndown <bjorndown@smoler.ch>
2023-09-04 12:10:43 +0200

    Add vercel adapter.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import solid from "solid-start/vite";
+import vercel from "solid-start-vercel";
 import { defineConfig } from "vite";
 
+const config = process.env.VERCEL ? { adapter: vercel() } : undefined;
+
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(config)],
 });
